775beb592c9263466f3caa6f636164965d8436b3
drawde <drawde@iMac-de-Web.local>
2015-11-06 18:58:45 -0500

    produccion audiovisual

--- audiovisual.php
+++ audiovisual.php
@@ -6,14 +6,32 @@
     <div class="content-one">
         <section class="content-menu">
             <div class="content-sub-menu">
-                <a>HOME</a>
-                <a>HOME</a>
-                <a>HOME</a>
-                <a>HOME</a>
+                <figure>
+                    <img src="images/pc-hover.png">
+                </figure>
+                <a>Reels</a>
+                <a>Video motion</a>
+                <a>Videos y comerciales tv</a>
+                <a>Fotografía</a>
+                <a>Multimedia</a>
+                <a>Animación y 3d</a>
             </div>
         </section>
         <section class="content-body">
-
+            <figure style="overflow: hidden; height: 250px;margin-top: 60px">
+                <img src="http://resources.wimpmusic.com/images/7f53e08b/9e07/4813/99cb/237388122847/2048x512.jpg" style="position: relative">
+                <p style="    position: absolute;
+    top: 200px;
+    font-size: 30px;
+    margin-left: 70px;
+    background-color: rgba(0, 0, 0, 0.28);
+    padding: 10px">PRODUCCIÓN AUDIOVISUAL</p>
+            </figure>
+            <div>
+                <figure>
+                    <img src="">
+                </figure>
+            </div>
         </section>
     </div>
 
