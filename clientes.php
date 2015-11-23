c3f3e7d949b04cd0a550b686d8779de751892764
drawde <drawde@iMac-de-Web.local>
2015-11-23 18:17:34 -0500

    haciendo clientes

--- clientes.php
+++ clientes.php
@@ -6,7 +6,46 @@
     <div class="client">
         <div class="wrapper-client">
         <h2>CLIENTES</h2>
-            <div></div>
+            <a>
+                <section>
+                    <figure>
+                        <img src="images/clientes/servientrega.png">
+                    </figure>
+                    <div>
+                        <p>cliente1</p>
+                    </div>
+                </section>
+            </a>
+            <a>
+                <section>
+                    <figure>
+                        <img src="images/clientes/servientrega.png">
+                    </figure>
+                    <div>
+                        <p>cliente1</p>
+                    </div>
+                </section>
+            </a>
+            <a>
+                <section>
+                    <figure>
+                        <img src="images/clientes/servientrega.png">
+                    </figure>
+                    <div>
+                        <p>cliente1</p>
+                    </div>
+                </section>
+            </a>
+            <a>
+                <section>
+                    <figure>
+                        <img src="images/clientes/servientrega.png">
+                    </figure>
+                    <div>
+                        <p>cliente1</p>
+                    </div>
+                </section>
+            </a>
         </div>
     </div>
 
