7a272b210850c0e20ea28d0756e0f67e526bba61
drawde <drawde@iMac-de-Web.local>
2015-11-27 11:33:45 -0500

    analitica

--- header.php
+++ header.php
@@ -2,16 +2,26 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Simon Sein</title>
+    <title>Agencia de Publicidad en Bogotá - Empresa de Publicidad</title>
     <link rel="stylesheet" href="css/flexslider.css" type="text/css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <meta name="author" content="Drawde"/>
-    <meta name="title" content="Agencia de publicidad Bogotá"/>
-    <meta name="description" content="Agencia de publicidad Bogotá" >
+    <meta name="title" content="Simon Sein-Diseño Web - Posicionamiento en Google-Video-Fotografía"/>
+    <meta name="description" content="Agencia de publicidad en Bogotá con 7 años de experiencia. En nuestra agencia de publicidad ofrecemos soluciones corporativas llenas de creatividad y tecnología"/>
     <meta name="keywords" content="Agencia de publicidad Bogotá">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="js/jquery.flexslider.js"></script>
     <script src="js/script.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-42627129-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -4,26 +4,11 @@
 
     <?php include('menu.php') ?>
     <h1 style="opacity: 0;height: 0;width: 0;display: block">Simon sein</h1>
-    <?php
-        $i=rand(1, 3);
-        echo $i;
-        if($i==1)
-        {
-            ?>
-            <video id="video" autoplay="autoplay" preload="auto" loop="loop" >
-                    <source src="images/fondo.mp4" type="video/mp4" />
-            </video>
-    <?php
-        }if($i==2){
-        ?>
+
         <video id="video" autoplay="autoplay" preload="auto" loop="loop" >
-            <source src="images/fondo1.mp4" type="video/mp4" />
+            <source src="images/fondo.mp4" type="video/mp4" />
         </video>
-        <?php
-        }if($i==3){
-        ?>
-        <div class="body-three"></div>
-        <?php
+
 
     }
     ?>
