7bdbd6ca11642548c101f2d1204057b86189fdfc
drawde <drawde@iMac-de-Web.local>
2015-11-27 11:19:02 -0500

    agregando google

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 
 
 </div>
-
+<a style="opacity: 0;height: 0;width: 0;display: block" class="fa fa-google-plus" style="font-size: 20px; " href="https://plus.google.com/u/0/111523482107785804073/posts" target="_blank" rel="publisher"></a>
 
 <?php include('footer.php'); ?>
 
